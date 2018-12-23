dfa138172084c3b3b26227eba17a5475b47fc1f0
BuGMaker'Bruce <oiiopro@live.cn>
2018-12-23 15:53:56 +0800

    first commit

---
+++ src/main/java/club/bugmakers/bruce/neteasejava/boyneteasejava/BoyNeteaseJavaApplication.java
@@ -0,0 +1,14 @@
+package club.bugmakers.bruce.neteasejava.boyneteasejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoyNeteaseJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoyNeteaseJavaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/club/bugmakers/bruce/neteasejava/boyneteasejava/BoyNeteaseJavaApplicationTests.java
@@ -0,0 +1,17 @@
+package club.bugmakers.bruce.neteasejava.boyneteasejava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoyNeteaseJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
