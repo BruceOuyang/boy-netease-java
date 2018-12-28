758df172aec5dcd02464b7537476b5d3f47f3eae
BuGMaker'Bruce <oiiopro@live.cn>
2018-12-28 09:30:34 +0800

    update 添加java运行原理示例代码

---
+++ src/test/java/club/bugmakers/boyneteasejava/demo/Demo1.java
@@ -0,0 +1,18 @@
+package club.bugmakers.boyneteasejava.demo;
+
+/**
+ * Demo1
+ *
+ * @Author Bruce
+ * @Date 2018/12/23 22:57
+ * @Version 1.0
+ **/
+public class Demo1 {
+    public static void main(String[] args) {
+        int x = 500;
+        int y = 100;
+        int a = x / y;
+        int b = 50;
+        System.out.println("a + b = " + (a + b));
+    }
+}
